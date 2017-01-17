cda3a802b2354a71fcc2aa1db176713d39449e2a
dionysus <dionysus.cho@gmail.com>
2017-01-17 01:00:16 -0800

    Initial Commit.  Working on making audio work.

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip [] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	void Awake () {
+
+		DontDestroyOnLoad (gameObject);
+		Debug.Log ("Don't Destroy onLoad: " + name);
+
+	}
+
+	void Start () {
+
+		audioSource = GetComponent<AudioSource> ();
+
+		if (audioSource){ Debug.Log ("audioSource was found");}
+	}
+
+	void OnEnable()
+	{
+		//Tell our 'OnLevelFinishedLoading' function to start listening for a scene change as soon as this script is enabled.
+		SceneManager.sceneLoaded += OnLevelFinishedLoading;
+	}
+
+	void OnDisable()
+	{
+		//Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change as soon as this script is disabled. Remember to always have an unsubscription for every delegate you subscribe to!
+		SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+	}
+
+	void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+	{
+		Debug.Log(scene.buildIndex);
+
+		int levelIndex = scene.buildIndex;
+		AudioClip thisLevelMusic = levelMusicChangeArray[levelIndex];
+
+		if (thisLevelMusic){ Debug.Log ("thisLevelMusic was found");}
+		else{ Debug.Log ("thisLevelMusic was not found");}
+
+
+		if (thisLevelMusic) { //if music is attached
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+
+
+	}
+
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+	public bool autoLoad = false;
+
+	void Start(){
+
+		if (autoLoad) {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+
+	public void LoadLevel(string name){
+		
+		SceneManager.LoadScene(name);
+	}
+
+	public void LoadNextLevel(){
+
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+}
